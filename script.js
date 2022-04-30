faf3e91d6f18036228af93d632b80edebebdcce2
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-30 21:11:13 +0530

    function to run animation

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const counter = document.querySelector(".counter");
 const finalMessage = document.querySelector(".final");
 const replay = document.querySelector("#replay");
 
+runAnimation();
+
 function resetDOM() {
   counter.classList.remove("hide");
   finalMessage.classList.remove("show");
@@ -12,4 +14,22 @@ function resetDOM() {
   });
 
   nums[0].classList.add("in");
+}
+
+function runAnimation() {
+  nums.forEach((num, idx) => {
+    const nextToLast = nums.length - 1;
+
+    num.addEventListener("animationend", (e) => {
+      if (e.animationName === "goIn" && idx !== nextToLast) {
+        num.classList.remove("in");
+        num.classList.add("out");
+      } else if (e.animationName === "goOut" && num.nextElementSibling) {
+        num.nextElementSibling.classList.add("in");
+      } else {
+        counter.classList.add("hide");
+        finalMessage.classList.add("show");
+      }
+    });
+  });
 }
