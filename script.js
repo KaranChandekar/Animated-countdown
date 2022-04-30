d5cbb3e5d29cfcbfb25fbe1116411b7847e72430
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-30 21:09:39 +0530

    function to reset DOM

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const nums = document.querySelectorAll(".nums span");
 const counter = document.querySelector(".counter");
 const finalMessage = document.querySelector(".final");
 const replay = document.querySelector("#replay");
+
+function resetDOM() {
+  counter.classList.remove("hide");
+  finalMessage.classList.remove("show");
+
+  nums.forEach((num) => {
+    num.classList.value = "";
+  });
+
+  nums[0].classList.add("in");
+}
