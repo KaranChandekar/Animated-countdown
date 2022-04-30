0b3a0a3b7a53ceae7a2cbccd9980d3c8e28a3daa
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-30 21:08:49 +0530

    grab elements with js

---
+++ script.js
@@ -0,0 +1,4 @@
+const nums = document.querySelectorAll(".nums span");
+const counter = document.querySelector(".counter");
+const finalMessage = document.querySelector(".final");
+const replay = document.querySelector("#replay");
